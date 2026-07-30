507149777fd8640f3c1ab8151564916edb5feefb
A0TD <yousef.y.m.d6@gmail.com>
2026-07-31 02:41:35 +0300

    👌 This update will add a simple logger Also separated app.ts and index.ts

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import swaggerUI from "swagger-ui-express";
+import specs from "./config/swaggerUIConfig.ts";
+import authRouter from "./router/authRoute.ts";
+import movieRouter from "./router/movieRoute.ts";
+import { requestLogger } from "./middleware/logger.ts";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(requestLogger);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+app.use("/auth", authRouter);
+
+app.use("/movies", movieRouter);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,19 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import swaggerUI from "swagger-ui-express";
-import specs from "./config/swaggerUIConfig.ts";
 import connectDB from "./config/mongoDB.ts";
-import authRouter from "./router/authRoute.ts";
-import movieRouter from "./router/movieRoute.ts";
+import app from "./app.ts";
 import "dotenv/config";
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+async function startServer() {
+  try {
+    await connectDB();
 
-app.use(express.json());
-app.use(cookieParser());
-
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-
-app.use("/auth", authRouter);
-
-app.use("/movies", movieRouter);
-
-connectDB()
-  .then(() => {
     app.listen(PORT, () => {
       console.log(`Listening on port ${PORT}`);
     });
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error("Couldn't connect to the database! " + error);
-  });
+  }
+}
+
+startServer();
